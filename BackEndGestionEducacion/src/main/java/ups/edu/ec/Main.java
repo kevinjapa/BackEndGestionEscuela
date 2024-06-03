bd42d427fb4d905b470483da0eeeab3dd28f2bb7
Paul-Astudillo <129395539+Paul-Astudillo@users.noreply.github.com>
2024-06-03 10:10:45 -0500

    Configuración del pom.xml

--- BackEndGestionEducacion/src/main/java/ups/edu/ec/Main.java
+++ BackEndGestionEducacion/src/main/java/ups/edu/ec/Main.java
@@ -2,7 +2,7 @@ package ups.edu.ec;
 
 public class Main {
     public static void main(String[] args) {
-        String Name= "Kevin";
+        String Name= "PPK";
         System.out.println(Name);
     }
 }
