a0c914abbd8dba6544125e247ade315a4296d61a
pedroxavier1451 <pedro.xavier.orellana@gmail.com>
2024-06-09 23:47:34 -0500

    Agregar PathBase y carpeta webapp

---
+++ BackEndGestionEducacion/src/main/java/ups/edu/ec/servicio/PathBase.java
@@ -0,0 +1,9 @@
+package ups.edu.ec.servicio;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("webservice")
+public class PathBase extends Application{
+
+}
